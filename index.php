6ec54e308b0bbd26c5c113e66dbdfe323d0d2e9e
Robert1GA <roberts.2015@yahoo.co.uk>
2017-05-21 15:26:27 -0700

    used template styles - may modify this some

--- index.php
+++ index.php
@@ -8,16 +8,25 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <link href="https://fonts.googleapis.com/css?family=Coda" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
     <title><?php echo $json_data['user']['username']; ?>'s Badges</title>
   </head>
   <body>
-    <?php
-      foreach ($courses_completed as $course) {
-        echo '<div>';
-        echo '<img src="' . $course["badge"] . '" />';
-        echo '<a href="' . $course["url"] . '">' . $course["title"] . '</a>';
-        echo '</div>';
-      }
-    ?>
+    <header>
+      <?php echo $json_data['user']['username']; ?>'s Badges
+    </header>
+    <div class="container">
+      <div class="grid">
+        <?php
+          foreach ($courses_completed as $course) {
+            echo '<div class="grid-cell">';
+            echo '<img class="badge" src="' . $course["badge"] . '" />';
+            echo '<a href="' . $course["url"] . '">' . $course["title"] . '</a>';
+            echo '</div>';
+          }
+        ?>
+      </div>
+    </div>
   </body>
 </html>
